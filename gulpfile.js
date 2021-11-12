fc08796622787ff855977cc43e6efafca3d8918f
airMay-hub <kiiikaaa.93@mail.ru>
2021-11-12 18:32:50 +0100

    feat: modify starter template

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,73 @@
-let project_folder = require("path").basename(__dirname);
-let source_folder = "src";
+let project_folder = require("path").basename(__dirname); // папка проекта
+let source_folder = "src"; // папка с исходными файлами
 
 let fs = require("fs");
+
+// пути к папкам и файлам
 
 let path = {
   build: {
     html: project_folder + "/",
     css: project_folder + "/css/",
     js: project_folder + "/js/",
-    img: project_folder + "/img/",
-    fonts: project_folder + "/fonts/",
+    img: project_folder + "/assets/img/",
+    video: project_folder + "/assets/video/",
+    audio: project_folder + "/assets/audio/",
+    fonts: project_folder + "/assets/fonts/",
   },
   src: {
-    html: [source_folder + "/*.html", "!"+source_folder + "/_*.html"],
-    css: source_folder + "/scss/style.scss",
+    html: [source_folder + "/*.html", "!"+ source_folder + "/_*.html"],
+    css: source_folder + "/style/style.scss",
     js: source_folder + "/js/script.js",
-    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
-    fonts: source_folder + "/fonts/*.ttf",
+    img: source_folder + "/assets/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    video: source_folder + "/assets/video/**/*.mp4",
+    audio: source_folder + "/assets/audio/**/*.mp3",
+    fonts: source_folder + "/assets/fonts/*.ttf",
   },
   watch: {
     html: source_folder + "/**/*.html",
-    css: source_folder + "/scss/**/*.scss",
+    css: source_folder + "/style/**/*.scss",
     js: source_folder + "/js/**/*.js",
-    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    img: source_folder + "/assets/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    video: source_folder + "/assets/video/**/*.mp4",
+    audio: source_folder + "/assets/audio/**/*.mp3",
   },
-  clean: "./" + project_folder + "/"
+  clean: "./" + project_folder + "/" // путь к папке проекта для функции удаления
 }
 
 let { src, dest } = require("gulp"),
   gulp = require("gulp"),
-  browsersync = require("browser-sync").create(),
+  browsersync = require("browser-sync").create(), // Live Server
+  // позволяет  с помощью @@include('путь к файлу') добавлять файлы в один основной (index.html, script.js)
   fileinclude = require("gulp-file-include"),
-  del = require("del"),
+  del = require("del"), // удаление
   scss = require("gulp-sass"),
-  autoprefixer = require("gulp-autoprefixer"),
-  group_media = require("gulp-group-css-media-queries"),
-  clean_css = require("gulp-clean-css"),
-  rename = require("gulp-rename"),
-  uglify = require("gulp-uglify"),
-  imagemin = require("gulp-imagemin"),
-  webp = require("gulp-webp"),
-  webphtml = require("gulp-webp-html"),
-  webpcss = require("gulp-webp-css"),
-  svgSprite = require("gulp-svg-sprite"),
-  ttf2woff = require("gulp-ttf2woff"),
-  ttf2woff2 = require("gulp-ttf2woff2"),
-  fonter = require("gulp-fonter");
+  autoprefixer = require("gulp-autoprefixer"), // автопрефиксер
+  group_media = require("gulp-group-css-media-queries"), // группирует медиа-запросы
+  clean_css = require("gulp-clean-css"), // минимизация стилей
+  rename = require("gulp-rename"), // переименование файлов
+  uglify = require("gulp-uglify"), // минимизация скрипта
+  imagemin = require("gulp-imagemin"), // минимизация изображений
+  webp = require("gulp-webp"), // перевод изображений в формат webp
+  webphtml = require("gulp-webp-html"), // подключение webp и jpg через тег picture
+  webpcss = require("gulp-webp-css"), // подключение webp в css
+  svgSprite = require("gulp-svg-sprite"), // спрайты
+  ttf2woff = require("gulp-ttf2woff"), //
+  ttf2woff2 = require("gulp-ttf2woff2"), //
+  fonter = require("gulp-fonter"); //
 
+// Функция, открывающая сервер
 function browserSync() {
   browsersync.init ({
     server: {
-      baseDir:  "./" + project_folder + "/"
+      baseDir: "./" + project_folder + "/"
     },
     port: 3000,
     notify: false
   })
 }
 
+// собирает html файлы
 function html () {
   return src(path.src.html)
   .pipe(fileinclude())
@@ -65,11 +76,12 @@ function html () {
   .pipe(browsersync.stream())
 }
 
+// собирает css файлы
 function css () {
   return src(path.src.css)
   .pipe(
     scss({
-      outputStyle: "compressed"
+      outputStyle: "expanded"
     })
   )
   .pipe(
@@ -93,6 +105,7 @@ function css () {
   .pipe(browsersync.stream())
 }
 
+// собирает js файлы
 function js () {
   return src(path.src.js)
   .pipe(fileinclude())
@@ -113,7 +126,7 @@ function images () {
   return src(path.src.img)
   .pipe (
     webp ({
-quality: 100
+      quality: 70
     })
   )
   .pipe(dest(path.build.img))
@@ -123,13 +136,26 @@ quality: 100
       progressive: true,
       svgoPlugins: [{ removeViewBox: false }],
       interlaced: true,
-      optimizationLevel: 1 // 0 to 7
+      optimizationLevel: 3 // 0 to 7
     })
   )
   .pipe(dest(path.build.img))
   .pipe(browsersync.stream())
 }
 
+function video () {
+  return src(path.src.video)
+  .pipe(dest(path.build.video))
+  .pipe(browsersync.stream())
+}
+
+function audio () {
+  return src(path.src.audio)
+  .pipe(dest(path.build.audio))
+  .pipe(browsersync.stream())
+}
+
+// обрабатывает шрифты
 function fonts() {
   src(path.src.fonts)
   .pipe(ttf2woff())
@@ -147,13 +173,14 @@ gulp.task("otf2ttf", function () {
     .pipe(dest(source_folder + "/fonts/"));
 })
 
+// для спрайтов сначала запустить gulp, потом в отдельном терминале gulp svgSprite
 gulp.task("svgSprite", function () {
   return gulp.src([source_folder + "/iconsprite/*.svg"])
   .pipe(svgSprite({
     mode: {
       stack: {
         sprite: "../icons/icons.svg",
-        // example: true
+        example: true
       }
     },
   }))
@@ -185,23 +212,29 @@ function cb() {
   
 }
 
+// следит за изменениями в файлах
 function watchFiles() {
   gulp.watch([path.watch.html], html);
   gulp.watch([path.watch.css], css);
   gulp.watch([path.watch.js], js);
   gulp.watch([path.watch.img], images);
+  gulp.watch([path.watch.video], video);
+  gulp.watch([path.watch.audio], audio);
 }
 
+// удаляет папку проекта, каждый раз при запуске gulp
 function clean() {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, video, audio, fonts), fontsStyle);
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 
 exports.fontsStyle = fontsStyle;
 exports.fonts = fonts;
+exports.video = video;
+exports.audio = audio;
 exports.images = images;
 exports.js = js;
 exports.css = css;
